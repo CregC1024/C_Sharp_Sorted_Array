375ea37909da3fb519a8ff3d4b8b391f29fe4114
CregC1024 <38873613+CregC1024@users.noreply.github.com>
2021-08-08 16:33:46 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C_Sharp_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 15, 16, 8, 6 };
+            Console.WriteLine("Original Arry");
+
+            foreach (var element in nums)
+                Console.WriteLine(element);
+
+            Array.Sort(nums);
+
+            Console.WriteLine("Sorted Array");
+
+            foreach (var element in nums)
+                Console.WriteLine(element);
+            Array.Reverse(nums);
+            Console.WriteLine("Reversed Array");
+            Array.ForEach<int>(nums, n => Console.WriteLine(n));
+            Console.WriteLine(Array.BinarySearch(nums, 15));
+
+        }
+    }
+}
